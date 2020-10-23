ebf434643f272dc668dc6076c1912efb0a58b587
HarshitPingo <48962963+pingo9o1@users.noreply.github.com>
2020-10-23 13:56:11 +0530

    Update GrayWorld-Color-Constancy.py

--- GrayWorld-Color-Constancy.py
+++ GrayWorld-Color-Constancy.py
@@ -24,17 +24,57 @@ def GRAYWORLD(nimg):
     return  nimg.transpose(1, 2, 0).astype(np.uint8)
 
 
-
-
+"""
 if __name__=="__main__":
 
     TrainImage = Image.open("Put Image" )
     #TrainImage.show()                                      #Sanity check for image        
     returnPil(GRAYWORLD(frompilImage(img))).show()                 
     
+   """ 
+    
+    #Running Gray World Algorithm for Multiple Images 
+
+
+    
+ #Here we have looped through all images iterativley and applied grayworld and saved in other location    
+    
+     
+import cv2
+import os
+
+def loadImages(path= "D:/Pingo/Project/train1/healthy"):
+
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".jpeg")]
     
     
     
+    """
+    temp= os.listdir(path)
+    images= []
+    for i in temp:
+        if(i.endswith("jpeg")):
+            images.append(i)
+    print(images)            #list of all images
+    """  
+
+filenames=loadImages()
+images= []
+
+for file in filenames:
+    images.append(cv2.imread(file))                  #will do in groups 
+    
+print(images)   
+    
+
+num=0
+
+for image in images:
+    
+    image=to_pil(grey_world(from_pil(image)))             //Grayworld Algorithm
+    #image.show()
+    cv2.imwrite('D:/Pingo/Project/saved2/unhealthy/'+ str(num) + " .jpeg" , np.float32(image))
+    num+=1
     
     
     
